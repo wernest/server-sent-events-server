0925c0f0cb0824a9634b570304310f584b0bab5d
will <willis127@gmail.com>
2016-04-21 07:40:06 -0700

    init commit

---
+++ src/main/java/org/glassfish/jersey/examples/sse/SampleConsumeClass.java
@@ -0,0 +1,13 @@
+package org.glassfish.jersey.examples.sse;
+
+/**
+ * Created by will on 4/20/16.
+ */
+public class SampleConsumeClass implements Runnable{
+
+
+    @Override
+    public void run() {
+
+    }
+}
